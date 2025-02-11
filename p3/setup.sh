4b3b94eb2fc5ba348eb4d8573a90bd0e7609b927
Naomi <naomi.hasalova@gmail.com>
2025-02-11 16:25:28 +0100

    P3 :)

--- p3/setup.sh
+++ p3/setup.sh
@@ -1,4 +1,18 @@
+# install docker
 # Add Docker's official GPG key:
+#!/bin/bash
+
+BASHRC="$HOME/.bashrc"
+ALIAS_LINE="alias k=kubectl"
+
+# Check if the alias is already present in .bashrc
+if grep -Fxq "$ALIAS_LINE" "$BASHRC"; then
+    echo "Alias already exists in $BASHRC"
+else
+    echo "$ALIAS_LINE" >> "$BASHRC"
+    echo "Alias added to $BASHRC. Please run 'source ~/.bashrc' to apply the changes."
+fi
+
 sudo apt-get update
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -13,3 +27,15 @@ echo \
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# install argocd cli
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
+#install k3d
+wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
---
+++ p3/start.sh
@@ -0,0 +1,27 @@
+NEW_PASSWORD="admin123"
+ARGOCD_SERVER="localhost:8080"
+
+
+k3d cluster delete iot
+k3d cluster create iot --port 8080:443@loadbalancer --port 8888:8888@loadbalancer 
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml 
+
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+
+
+
+
+kubectl wait --namespace argocd --for=condition=ready pod --all --timeout=300s
+INITIAL_PASSWORD=$(argocd admin initial-password -n argocd | head -1)
+
+argocd login $ARGOCD_SERVER --username admin --password $INITIAL_PASSWORD --insecure
+
+argocd account update-password \
+  --current-password "${INITIAL_PASSWORD}" \
+  --new-password "${NEW_PASSWORD}" \
+  --server ${ARGOCD_SERVER}
+
+kubectl create namespace dev
+kubectl apply -f argocd-app.yaml
